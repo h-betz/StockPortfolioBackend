e35e30160094210b25abe1f82984991be34d0023
h-betz <hhb311@gmail.com>
2017-09-18 19:40:46 -0400

    Code clean up. Simple CRUD working

--- stock_app/views.py
+++ stock_app/views.py
@@ -1,54 +1,21 @@
 """
 Some DRF stuff
 """
-from rest_framework import status
-from rest_framework.response import Response
-from rest_framework.views import APIView
-from django.http import Http404
+from rest_framework import generics
 from .models import Stock
 from .serializers import StockSerializer
 
 # Create your views here.
-class StockList(APIView):
+class StockList(generics.ListCreateAPIView):
     """
     List all code snippets, or create a new snippet.
     """
-    def get(self, request, format=None):
-        stocks = Stock.objects.all()
-        serializer = StockSerializer(stocks, many=True)
-        return Response(serializer.data)
-    
-    def post(self, request, format=None):
-        serializer = StockSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    queryset = Stock.objects.all()
+    serializer_class = StockSerializer
 
-class StockDetail(APIView):
+class StockDetail(generics.RetrieveUpdateDestroyAPIView):
     """
     Retrieve, update or delete a code snippet.
     """
-    def get_object(self, pk):
-        try:
-            return Stock.objects.get(pk=pk)
-        except Stock.DoesNotExist:
-            raise Http404
-    
-    def get(self, request, pk, format=None):
-        stock = self.get_object(pk)
-        serializer = StockSerializer(stock)
-        return Response(serializer.data)
-
-    def put(self, request, pk, format=None):
-        stock = self.get_object(pk)
-        serializer = StockSerializer(stock, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_404_NOT_FOUND)
-
-    def delete(self, request, pk, format=None):
-        stock = self.get_object(pk)
-        stock.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+    queryset = Stock.objects.all()
+    serializer_class = StockSerializer
